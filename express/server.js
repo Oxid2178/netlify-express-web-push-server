4d4040c3d8413f65a604be4605bbc057648a6c05
Antonio Carlucci <antonio.carlucci@bbv.ch>
2020-10-07 15:38:50 +0200

    Add example with r/w file

--- express/server.js
+++ express/server.js
@@ -6,6 +6,24 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const webpush = require("web-push");
+const fs = require("fs");
+
+console.log("Going to write into existing file");
+// Open a new file with name input.txt and write Simply Easy Learning! to it.
+fs.writeFile('input.txt', 'Simply Easy Learning!', function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Data written successfully!");
+   console.log("Let's read newly written data");
+   // Read the newly written file and print all of its content on the console
+   fs.readFile('input.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("Asynchronous read: " + data.toString());
+   });
+});
 
 app.use(cors()); // allow access from any origin ! important must be declared here on top !
 
